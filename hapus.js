51cf8629068d39f273f44c994305d49b8716e88d
Salman <kingmansalman@gmail.com>
2025-08-05 05:58:11 +0700

    delete: <food>

--- hapus.js
+++ hapus.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import foodModel from './models/foodModel';
 
-await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect(process.env.MONGODB_URI);
 await foodModel.deleteMany({});
 console.log("Semua data di collection 'foods' sudah dihapus.");
